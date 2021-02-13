80a6ba8adbba09a24f95e17478b059fde2d7c391
chanhl22 <chanhlee@student.42seoul.kr>
2021-02-13 19:36:08 +0900

    try catch with setTimeout

--- trycatch.js
+++ trycatch.js
@@ -5,10 +5,26 @@
 //     alert('error');
 // }
 
-try {
-    alert('try 블록 시작');  // (1) <--
-    lalala; // 에러, 변수가 정의되지 않음!
-    alert('try 블록 끝(절대 도달하지 않음)');  // (2)
-} catch (err) {
-    alert(`에러가 발생했습니다!`); // (3) <--
+// try {
+//     alert('try 블록 시작');  // (1) <--
+//     lalala; // 에러, 변수가 정의되지 않음!
+//     alert('try 블록 끝(절대 도달하지 않음)');  // (2)
+// } catch (err) {
+//     alert(`에러가 발생했습니다!`); // (3) <--
+// }
+
+function wait(sec) {
+    return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            reject('error');
+        }, sec * 1000);
+    });
 }
+
+wait(3)
+    .catch (e => {
+        console.log('1st catch', e);
+    })
+    .catch (e => {
+        console.log('2st cathc', e);
+    });
